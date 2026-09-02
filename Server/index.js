7b204132668424e92d0a6a91fe102f000208656d
Arya Baranwal <baranwalarya414@gmail.com>
2026-09-02 23:57:52 +0530

    Initial Commit

---
+++ Server/index.js
@@ -0,0 +1,16 @@
+ import express from "express"
+ import dotenv from "dotenv"
+ dotenv.config()
+
+ const app=express()
+
+ const PORT=process.env.PORT
+
+ app.get("/",(req,res)=>{
+    res.json("Hello from Server")
+ })
+
+ app.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT}`)
+ })
+
